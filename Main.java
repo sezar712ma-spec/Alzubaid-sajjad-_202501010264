f886f25853e24fe65882f7e1dba3a4d87f47a99c
sezar712ma-spec <sezar712ma@gmail.com>
2026-06-09 05:07:18 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Sajjad", 21, 3.8);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    
+    }
+}

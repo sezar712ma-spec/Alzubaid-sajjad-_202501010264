2855a2f52e369a82bd0fdd555b4811507fb68eb6
sezar712ma-spec <sezar712ma@gmail.com>
2026-06-24 14:27:30 +0800

    Add average calculation for student quizzes

--- week_3/Student_Ave_Calc.py
+++ week_3/Student_Ave_Calc.py
@@ -1,1 +1,20 @@
+choice = "Y"
 
+while choice == "Y":
+
+    quiz_1 = float(input("Enter Quiz 1 mark: "))
+    quiz_2 = float(input("Enter Quiz 2 mark: "))
+    quiz_3 = float(input("Enter Quiz 3 mark: "))
+
+    average = (quiz_1 + quiz_2 + quiz_3) / 3
+
+    print("Average =", average)
+
+    if average >= 50:
+        print("Pass")
+    else:
+        print("Fail")
+
+    choice = input("Continue? Select Y/N: ")
+
+print("Program Ended")
